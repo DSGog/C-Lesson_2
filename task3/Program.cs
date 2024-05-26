fb15141daaf33c47af4a878f547f4961e012760f
DSGog <dmitriy.amsec@gmail.com>
2024-05-26 20:50:49 +0400

    Lesson2: while, for, foreach

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// int n = 10;
+int[] arr = { 4, 3, 5, 6, 1, 2, 8, 7, 9, 10 };
+int i = 0;
+
+while (i < arr.Length)
+{
+     if (arr[i] % 2 == 0)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
+   
---
+++ task4/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] array = { 1, 2, 4, 8, 98 };
+int i = 0;
+int max = array[0];
+
+while (i < array.Length)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+System.Console.WriteLine(max);
+
+for (int j = 1; j < array.Length; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+
+System.Console.WriteLine($"Максимальное значение: {max}");
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+System.Console.WriteLine($"Foreach: {max}");
